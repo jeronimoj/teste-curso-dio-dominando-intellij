4076e00c3c1b837102324ee730896979c3f38483
Jerónimo Paulo Pinto <dev4life228@gmail.com>
2022-03-06 12:31:21 +0100

    test done

--- src/br/com/dio/PrimeroPrograma.java
+++ src/br/com/dio/PrimeroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import model.Gato;
 
+import java.lang.reflect.Constructor;
+
 public class PrimeroPrograma {
     public static void main(String[] args){
         /*int a=5;
@@ -10,5 +12,40 @@ public class PrimeroPrograma {
         System.out.println("Hello world" +(a+b));*/
         Gato gato = new Gato();
         System.out.println("..."+gato);
+        Livro livro = new Livro("O sonho", 200);
+        System.out.println("..."+livro);
+
+    }
+}
+class Livro{
+    private String nome;
+    private  Integer numPagina;
+
+    public Livro(String nome, Integer numPagina){
+        this.nome = nome;
+        this.numPagina = numPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getNumPagina(){
+        return numPagina;
+    }
+
+    public void setNumPagina(Integer numPagina) {
+        this.numPagina = numPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagina=" + numPagina +
+                '}';
     }
 }
