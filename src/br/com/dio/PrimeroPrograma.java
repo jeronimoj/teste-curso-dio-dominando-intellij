acbe948c222d2963c081f883b2ccf8c3fad9c617
Jerónimo Paulo Pinto <dev4life228@gmail.com>
2022-03-06 12:07:49 +0100

    first commit

---
+++ src/br/com/dio/PrimeroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeroPrograma {
+    public static void main(String[] args){
+        /*int a=5;
+        int b=3;
+
+        System.out.println("Hello world" +(a+b));*/
+        Gato gato = new Gato();
+        System.out.println("..."+gato);
+    }
+}
